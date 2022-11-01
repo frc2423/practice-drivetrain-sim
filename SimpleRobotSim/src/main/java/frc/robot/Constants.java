203b5c8f24acc30edc7b96f269bcbd44746c63f0
amorygalili <amorygalili@gmail.com>
2022-10-31 22:24:53 -0400

    Step 1

---
+++ SimpleRobotSim/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public static final double WHEEL_RADIUS = 0.0508; // meters
+    public static final int ENCODER_RESOLUTION = 4096;
+
+    public static final String GO_FORWARD_AUTO = "Go Forward";
+    public static final String TRAVEL_PATH_AUTO = "Travel Path";
+
+    public static final double KvLinear = 1.98;
+    public static final double KaLinear = 0.2;
+    public static final double KvAngular = 1.5;
+    public static final double KaAngular = 0.3;
+}
